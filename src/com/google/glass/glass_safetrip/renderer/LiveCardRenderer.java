5d7c66436269b87dc0831adeb77220031a83e8cc
Manolo Vera <manolo@droiders.com>
2013-11-20 13:20:57 +0100

    ADDED: live card renderer

---
+++ src/com/google/glass/glass_safetrip/renderer/LiveCardRenderer.java
@@ -0,0 +1,34 @@
+package com.google.glass.glass_safetrip.renderer;
+
+import android.view.SurfaceHolder;
+import com.google.android.glass.timeline.LiveCardCallback;
+
+/**
+ * glass
+ * com.google.glass.glass_safetrip.renderer
+ *
+ * @autor manolo
+ */
+public class LiveCardRenderer implements LiveCardCallback {
+
+    @Override
+    public void renderingPaused(SurfaceHolder surfaceHolder, boolean b) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}
