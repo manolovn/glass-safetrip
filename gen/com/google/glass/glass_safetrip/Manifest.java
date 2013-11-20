80dfa4b453749e866ddf9e53c3a5a93fe86e69c6
Manolo Vera <manolo@droiders.com>
2013-11-20 11:00:58 +0100

    first version

---
+++ gen/com/google/glass/glass_safetrip/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.google.glass.glass_safetrip;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
